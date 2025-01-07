c80d51d1752244df6b7e712aa0b7f409d37432c2
Eric Charnesky <EricCharnesky@gmail.com>
2025-01-07 12:35:34 -0500

    lab 0

---
+++ Lab0/main.py
@@ -0,0 +1,4 @@
+print('Eric Charnesky, Doctoral Drop Out')
+print('Eric teaches at OCC and Walsh, likes to run, and play D&D')
+print('Eric is an Eagle Scout')
+print('The coolest thing you have seen tech do')
